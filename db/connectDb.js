9bc7ba6a9009df9c544044f54d6b974bd30af4b1
SaiSurendar43 <saisivas200@gmail.com>
2025-02-17 18:30:35 +0530

    commit the changes

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-export const connectDb = async () =>{
-     try{
-       const conn = await mongoose.connect(process.env.MONGO_URI);
-       console.log(`MongoDB Connected: ${conn.connection.host}`);
-   }
-     catch(error){
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-     }
-}
+export const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Reduce timeout
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
